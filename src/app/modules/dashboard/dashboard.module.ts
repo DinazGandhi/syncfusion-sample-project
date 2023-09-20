661b48967da67077d82b9651420d7b5b94d41b37
dinaz.gandhi@capium.com <dinaz.gandhi@capium.com>
2023-09-20 10:41:40 +0530

    Implement changes for Task Wise Hours Details chart and Timer Widget

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,8 +1,13 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
 import { NgSelectModule } from '@ng-select/ng-select';
+import {
+  ChartModule
+} from '@syncfusion/ej2-angular-charts';
 import { DashboardLayoutModule } from '@syncfusion/ej2-angular-layouts';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -15,8 +20,12 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     CommonModule,
     DashboardRoutingModule,
     DashboardLayoutModule,
+    ChartModule,
     MatIconModule,
+    MatSelectModule,
+    FormsModule,
     NgSelectModule
-  ]
+  ],
+  providers: [DatePipe]
 })
 export class DashboardModule { }
--- src/app/modules/dashboard/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard/dashboard.component.ts
@@ -1,3 +1,4 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 export enum DashboardWidget {
@@ -35,82 +36,31 @@ export class DashboardComponent implements OnInit {
   feesDashboardSummary: any;
   mostLeastCount: any;
 
-  constructor() {
-    this.userWidgetsList = [
-      {
-        widgetId: 1,
-        widgetTypeName: 'Time Section Charts',
-        widgetTypeId: 1,
-        widgetName: 'Summary',
-        isEnable: true,
-        sortOrder: 1,
-        iconName: null,
-      },
-      {
-        widgetId: 2,
-        widgetTypeName: 'Time Section Charts',
-        widgetTypeId: 1,
-        widgetName: 'Most vs. Least Counts',
-        isEnable: true,
-        sortOrder: 2,
-        iconName: 'assets/images/column-chart.svg',
-      },
-      {
-        widgetId: 3,
-        widgetTypeName: 'Time Section Charts',
-        widgetTypeId: 1,
-        widgetName: 'Task Wise Hours Details',
-        isEnable: true,
-        sortOrder: 3,
-        iconName: 'assets/images/column-chart.svg',
-      },
-      {
-        widgetId: 4,
-        widgetTypeName: 'Time Section Charts',
-        widgetTypeId: 1,
-        widgetName: 'Timer Widget',
-        isEnable: true,
-        sortOrder: 4,
-        iconName: 'assets/images/pie-chart.svg',
-      },
-      {
-        widgetId: 10,
-        widgetTypeName: 'Fees Section Charts',
-        widgetTypeId: 2,
-        widgetName: 'Fees Summary',
-        isEnable: true,
-        sortOrder: 10,
-        iconName: null,
-      },
-      {
-        widgetId: 11,
-        widgetTypeName: 'Fees Section Charts',
-        widgetTypeId: 2,
-        widgetName: 'Invoice Amount by Invoice Month',
-        isEnable: true,
-        sortOrder: 11,
-        iconName: 'assets/images/column-chart.svg',
-      },
-      {
-        widgetId: 12,
-        widgetTypeName: 'Fees Section Charts',
-        widgetTypeId: 2,
-        widgetName: 'Balance Amount by Due Month',
-        isEnable: true,
-        sortOrder: 12,
-        iconName: '',
-      },
-      {
-        widgetId: 13,
-        widgetTypeName: 'Fees Section Charts',
-        widgetTypeId: 2,
-        widgetName: 'Invoice Amount by Category',
-        isEnable: true,
-        sortOrder: 12,
-        iconName: '',
-      },
-    ];
+  //#region Task Wise Hours Chart
+  public primaryXAxis?: Object;
+  public chartData?: Object[];
+  public title?: string;
+  primaryYAxis: any;
+  public tooltip?: Object = {
+    enable: true,
+    format: '<span>${point.x}</span> : <span>${point.y}h</span>',
+  };
+  public palette?: string[];
+  public legend: Object = {
+    visible: false,
+    enableHighlight: true,
+  };
 
+  seriesData: any = [];
+  dashboardRequestModel: any;
+  filter = 1;
+  start: string = '';
+  end: string = '';
+  taskWiseHoursDetails: any;
+  taskId = '';
+  //#endregion
+
+  constructor(public datepipe: DatePipe) {
     this.timeDashboardSummary = {
       totalAvailableClients: 3,
       totalWorkingClients: 0,
@@ -174,12 +124,157 @@ export class DashboardComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.addWidgetClick();
+
     setTimeout(() => {
-      window.dispatchEvent(new Event('resize'));
-    }, 800);
+      this.getTaskWiseHoursDetails();
+      // window.dispatchEvent(new Event('resize'));
+    }, 1000);
   }
 
   onDragStop(event: any): void {}
 
   onCloseIcon(widget: any, event: any): void {}
+
+  addWidgetClick(): void {
+    // setTimeout(() => {
+    this.userWidgetsList = [
+      {
+        widgetId: 1,
+        widgetTypeName: 'Time Section Charts',
+        widgetTypeId: 1,
+        widgetName: 'Summary',
+        isEnable: true,
+        sortOrder: 1,
+        iconName: null,
+      },
+      {
+        widgetId: 2,
+        widgetTypeName: 'Time Section Charts',
+        widgetTypeId: 1,
+        widgetName: 'Most vs. Least Counts',
+        isEnable: true,
+        sortOrder: 2,
+        iconName: 'assets/images/column-chart.svg',
+      },
+      {
+        widgetId: 3,
+        widgetTypeName: 'Time Section Charts',
+        widgetTypeId: 1,
+        widgetName: 'Task Wise Hours Details',
+        isEnable: true,
+        sortOrder: 3,
+        iconName: 'assets/images/column-chart.svg',
+      },
+      {
+        widgetId: 4,
+        widgetTypeName: 'Time Section Charts',
+        widgetTypeId: 1,
+        widgetName: 'Timer Widget',
+        isEnable: true,
+        sortOrder: 4,
+        iconName: 'assets/images/pie-chart.svg',
+      },
+      {
+        widgetId: 10,
+        widgetTypeName: 'Fees Section Charts',
+        widgetTypeId: 2,
+        widgetName: 'Fees Summary',
+        isEnable: true,
+        sortOrder: 10,
+        iconName: null,
+      },
+      {
+        widgetId: 16,
+        widgetTypeName: 'Fees Section Charts',
+        widgetTypeId: 2,
+        widgetName: 'Income Trend',
+        isEnable: true,
+        sortOrder: 16,
+        iconName: '',
+      },
+      {
+        widgetId: 17,
+        widgetTypeName: 'Fees Section Charts',
+        widgetTypeId: 2,
+        widgetName: 'Revenue Flow',
+        isEnable: true,
+        sortOrder: 17,
+        iconName: '',
+      },
+      {
+        widgetId: 13,
+        widgetTypeName: 'Fees Section Charts',
+        widgetTypeId: 2,
+        widgetName: 'Invoice Amount by Category',
+        isEnable: true,
+        sortOrder: 12,
+        iconName: '',
+      },
+    ];
+    // }, 1000);
+  }
+
+  getTaskWiseHoursDetails(): void {
+    //#region  Task Wise Hours Chart Details
+    this.palette = ['#B9DD94', '#93CAF0', '#F1D071', '#C4C24A'];
+    this.primaryXAxis = {
+      interval: 1,
+      valueType: 'Category',
+    };
+
+    this.taskWiseHoursDetails = {
+      totalHours: '27:00',
+      totalBillableHours: '24:00',
+      totalNonBillableHours: '03:00',
+      data: [
+        {
+          taskId: '6b1fb5fe-a32b-ee11-a9c1-00224801b1f2',
+          name: 'Accounts production ',
+          data: [
+            {
+              x: 'Monday',
+              y: 15,
+              tooltipMappingName: null,
+            },
+            {
+              x: 'Tuesday',
+              y: 12,
+              tooltipMappingName: null,
+            },
+            {
+              x: 'Wednesday',
+              y: 0,
+              tooltipMappingName: null,
+            },
+            {
+              x: 'Thursday',
+              y: 0,
+              tooltipMappingName: null,
+            },
+            {
+              x: 'Friday',
+              y: 0,
+              tooltipMappingName: null,
+            },
+            {
+              x: 'Saturday',
+              y: 0,
+              tooltipMappingName: null,
+            },
+          ],
+        },
+      ],
+    };
+
+    if (this.taskWiseHoursDetails) {
+      this.seriesData = this.taskWiseHoursDetails.data;
+    } else {
+      this.seriesData = [];
+    }
+
+    window.dispatchEvent(new Event('resize'));
+
+    //#endregion
+  }
 }
